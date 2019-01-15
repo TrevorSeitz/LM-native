8f9d8ef223ab20d6ac522e8f822cc4c471e79575
TrevorSeitz <trevinla@gmail.com>
2019-01-15 14:55:20 -0500

    newPlaceScreen showing camera switch

--- App.js
+++ App.js
@@ -34,52 +34,6 @@ class Detail extends Component {
     );
   }
 }
-//
-// const FeedStack = createStackNavigator(
-//   {
-//     Feed: {
-//       screen: Feed,
-//       navigationOptions: ({ navigation }) => {
-//         return {
-//           headerTitle: "Feed",
-//           headerLeft: (
-//             <Icon
-//               style={{ paddingLeft: 10 }}
-//               onPress={() => navigation.openDrawer()}
-//               name="md-menu"
-//               size={30}
-//             />
-//           )
-//         };
-//       }
-//     },
-//     Detail: { screen: Detail }
-//   },
-//   {
-//     defaultNavigationOptions: {
-//       gesturesEnabled: false
-//     }
-//   }
-// );
-
-// const SettingsStack = createStackNavigator({
-//   Settings: {
-//     screen: Settings,
-//     navigationOptions: ({ navigation }) => {
-//       return {
-//         headerTitle: "Settings",
-//         headerLeft: (
-//           <Icon
-//             style={{ paddingLeft: 10 }}
-//             onPress={() => navigation.openDrawer()}
-//             name="md-menu"
-//             size={30}
-//           />
-//         )
-//       };
-//     }
-//   }
-// });
 
 const AppSwitchNavigator = createSwitchNavigator({
   Welcome: { screen: WelcomeScreen },
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,36 +1,52 @@
 import * as React from "react";
-import { Button, Text, View, StyleSheet, Switch } from "react-native";
+import { Text, View, StyleSheet, Switch } from "react-native";
 import { Constants, MapView } from "expo";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 import { Card } from "react-native-paper";
 
 export default class NewPlaceScreen extends React.Component {
+  state = { switchValue: false };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>hello World - new place screen</Text>
-        <View style={{ flex: 1 }}>
-          <View style={styles.switchView}>
-            <Text>Show Camera</Text>
-            <Switch
-              onValueChange={value => {
-                this.setState({ switchValue: value });
-              }}
-              value={this.state.switchValue}
-              style={styles.switch}
-            />
-          </View>
-          {this.state.switchValue ? (
-            <View style={styles.cameraview}>
-              <Text>Camera on</Text>
-            </View>
-          ) : (
-            <View style={styles.cameraview}>
-              <Text>Camera off</Text>
-            </View>
-          )}
+        <View style={styles.switchView}>
+          <Text>Show Camera</Text>
+          <Switch
+            onValueChange={value => {
+              this.setState({ switchValue: value });
+            }}
+            value={this.state.switchValue}
+            style={styles.switch}
+          />
         </View>
+        {this.state.switchValue ? (
+          <View style={styles.cameraview}>
+            <Text>Camera on</Text>
+          </View>
+        ) : (
+          <View style={styles.cameraview}>
+            <Text>Camera off</Text>
+          </View>
+        )}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
+});

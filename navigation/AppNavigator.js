dcd44957d198e8a2ccb220ca01fbdfd22a918f2f
TrevorSeitz <trevinla@gmail.com>
2019-01-18 14:20:13 -0500

    add comments

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -7,19 +7,20 @@ import {
 
 import MainTabNavigator from "./MainTabNavigator";
 import NewFormNavigator from "./NewFormNavigator";
-// import LMImagePickerNavigator from "./LMImagePickerNavigator";
+// import LMImagePickerNavigator from "./";
 
 const AppNavigator = createStackNavigator({
+  // this sets up the screens for navigation
   Home: { screen: MainTabNavigator },
   New: { screen: NewFormNavigator },
   LMImagePicker: { screen: LMImagePickerNavigator }
 });
 
 const AppContainer = createAppContainer(appNavigator);
+// this is what will be displayed in App.js
 <AppContainer
   onNavigationStateChange={handleNavigationChange}
   uriPrefix="/app"
 />;
-// export default AppNavigator;
-// export default createAppContainer(AppNavigator);
+
 export default AppContainer;

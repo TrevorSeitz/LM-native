c30c04961fb81587e9adee121e74da20bea95bdd
Trevor Seitz <trevinla@gmail.com>
2019-01-31 17:19:45 -0500

    checking for unused files

---
+++ screens/auth/SignupScreen.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { StyleSheet, View, Text } from "react-native";
+
+
+export default class SignupScreen extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Text style={{paddingTop: 20}}>SignupScreen</Text>
+  }
+}
+const styles = StyleSheet.create({})

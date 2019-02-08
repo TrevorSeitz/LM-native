863d5c848e4bd5f12f8d8ce7cf34d096c8864576
TrevorSeitz <trevinla@gmail.com>
2019-02-08 13:31:52 -0500

    comment cleanup

--- App.js
+++ App.js
@@ -11,9 +11,6 @@ import { createAppContainer } from "react-navigation";
 import ApiKeys from "./constants/ApiKeys";
 import * as firebase from "firebase";
 import { Constants } from "expo";
-//
-// import { MaterialIcons } from "@expo/vector-icons";
-// import { Font, AppLoading } from "expo";
 
 import LoginSwitchNavigator from "./navigation/switches/LoginSwitchNavigator";
 import AppSwitchNavigator from "./navigation/switches/AppSwitchNavigator";
@@ -31,50 +28,25 @@ export default class App extends React.Component {
     };
   }
 
-  // Initialize firebase
-  //   if (!firebase.apps.length) {
-  //     firebase.initializeApp(ApiKeys.firebaseConfig); //does this actually belong in ApiKeys?
-  //     firebase.auth().onAuthStateChanged(this.StateChanged);
-  //     var database = firebase.database();
-  //   }
-  // }
-
   componentDidMount() {
     this.authListener();
   }
 
   authListener() {
     firebase.auth().onAuthStateChanged(user => {
-      // console.log("state change", user);
       if (user) {
         this._storeData(user);
-        this.setState({ user });
-        // firebase.User = user;
+        this.setState({ user, uid: user.uid });
       } else {
         this.setState({ user: null });
       }
     });
-    // this._storeData();
   }
 
   _storeData = async user => {
     try {
       await AsyncStorage.setItem("uid", user.uid);
-    } catch (error) {
-      // Error saving data
-    }
-    this._retrieveData();
-  };
-
-  _retrieveData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("uid");
-      if (value !== null) {
-        this.setState({ uid: value });
-      }
-    } catch (error) {
-      // Error retrieving data
-    }
+    } catch (error) {}
   };
 
   render() {
@@ -93,14 +65,6 @@ const AppContainer = createAppContainer(AppSwitchNavigator);
 const LoginContainer = createAppContainer(LoginSwitchNavigator);
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  //   borderRadius: 4,
-  //   borderWidth: 0.5,
-  //   borderColor: "#d6d7da"
-  // }
   container: {
     flex: 1,
     justifyContent: "center",

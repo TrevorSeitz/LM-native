d5c4d6ca0ca3036351455f59f1ba65b8ea6d818d
TrevorSeitz <trevinla@gmail.com>
2019-03-07 16:30:06 -0500

    still some issues loading additional photos after they are added - the photos are not fully saved before going to the view page

--- stacks/EditLocationStack.js
+++ stacks/EditLocationStack.js
@@ -11,16 +11,16 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import Icon from "@expo/vector-icons/Ionicons";
 import { Constants, MapView } from "expo";
 
-import HomeScreen from "../screens/HomeScreen";
-import ListLocationsScreen from "../screens/ListLocationsScreen";
-import LocationDetailsScreen from "../screens/LocationDetailsScreen";
+// import HomeScreen from "../screens/HomeScreen";
+// import ListLocationsScreen from "../screens/ListLocationsScreen";
+// import LocationDetailsScreen from "../screens/LocationDetailsScreen";
 import EditLocationScreen from "../screens/EditLocationScreen";
 import EditAdditionalPhotosScreen from "../screens/EditAdditionalPhotosScreen";
 import AdditionalImageBrowser from "../screens/AdditionalImageBrowser";
 
-import EditAdditionalPhotosStack from "../stacks/EditAdditionalPhotosStack";
+// import EditAdditionalPhotosStack from "../stacks/EditAdditionalPhotosStack";
 
-import EditAdditionalPhotosSwitchNavigator from "../navigation/switches/EditAdditionalPhotosSwitchNavigator";
+// import EditAdditionalPhotosSwitchNavigator from "../navigation/switches/EditAdditionalPhotosSwitchNavigator";
 
 const EditLocationStack = createStackNavigator(
   {
